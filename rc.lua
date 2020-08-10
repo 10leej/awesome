fbfbbc2bac02ba501d7ba32fe439799b803363d4
Joshua Lee <josh@10leej.com>
2020-08-09 23:59:36 -0400

    eh we don't need that in our lives right now

--- rc.lua
+++ rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init("/home/joshua/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "termite"
@@ -508,7 +508,7 @@ awful.rules.rules = {
           "Kruler",
           "MessageWin",  -- kalarm.
           "Sxiv",
-          "Thunar", --tiling is great but I have a massive screen so this is nicer to have floating
+--nah          "Thunar", --tiling is great but I have a massive screen so this is nicer to have floating
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
@@ -528,7 +528,7 @@ awful.rules.rules = {
 
     -- Add titlebars to normal clients and dialogs
     { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
+      }, properties = { titlebars_enabled = false }
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.

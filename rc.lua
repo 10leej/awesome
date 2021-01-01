294ce6d177ef3136b847ffb2dbab3a30e8306d5a
Joshua Lee <josh@10leej.com>
2021-01-01 03:05:03 -0500

    test

--- rc.lua
+++ rc.lua
@@ -633,7 +633,7 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
-
+--test
 --Autostart
 awful.util.spawn("picom")
 --awful.util.spawn("light-locker")
